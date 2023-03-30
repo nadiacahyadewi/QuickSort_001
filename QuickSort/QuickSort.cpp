b16b74208e57e8dccb9db271544743864cc59976
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-03-30 10:39:40 +0700

    membuat array untuk menyimpan bilangan bulat

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 //array of integers to hold values
-int arr[20];
-int cmp_count = 0;
-int mov_count = 0;
+int arr[20]; 
+int cmp_count = 0; // number of comprasion
+int mov_count = 0; // number of data movements
 int n;
 
 void input() {
