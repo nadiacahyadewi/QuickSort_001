dbc0cb2ba98919fea3b34e37e7115b111e6b96a3
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-03-30 10:37:06 +0700

    Add project files.

---
+++ QuickSort/QuickSort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//array of integers to hold values
+int arr[20];
+int cmp_count = 0;
+int mov_count = 0;
+int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukan panjang element array: ";
+		cin >> n;
+
+		if (n <=20)
+	}
+}
