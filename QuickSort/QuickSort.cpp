8c06d066564f2314983d44705054787c1380caac
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-03-30 11:49:07 +0700

    membuat fungsi untuk memulai program

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -110,3 +110,16 @@ void display() {
 	cout << "\n\nNumber of comparasions: " << cmp_count << endl;
 	cout << "Number of data movement: " << mov_count << endl;
 }
+
+
+int main()
+{
+
+	input();
+	//Sort the array using quick sort
+	q_short(0, n - 1);
+	display();
+	system("pause");
+
+	return 0;
+}
