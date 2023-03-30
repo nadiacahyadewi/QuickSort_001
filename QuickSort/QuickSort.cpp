481e20c9db1b1366028793dc8cbbb2018b78f1f8
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-03-30 10:54:49 +0700

    memasukan panjang array

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -13,6 +13,19 @@ void input() {
 		cout << "Masukan panjang element array: ";
 		cin >> n;
 
-		if (n <=20)
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+
+	cout << "\n------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[1];
 	}
 }
